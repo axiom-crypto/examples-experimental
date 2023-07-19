7d21f301b92d0ae54c2a872b3d4feed4e73dafbc
Yu Jiang Tham <yoojbruin@gmail.com>
2023-07-19 18:23:47 +0200

    Update abi and impl

--- quickstart/index.ts
+++ quickstart/index.ts
@@ -2,11 +2,13 @@ import { Axiom, AxiomConfig } from '@axiom-crypto/experimental';
 import { ReceiptField } from '@axiom-crypto/experimental/experimental/onlyReceiptsQueryBuilder';
 import { TransactionField } from '@axiom-crypto/experimental/experimental/txReceiptsQueryBuilder';
 import { ethers } from 'ethers';
+import axiomV1QueryExperimentalAbi from './abi/AxiomExperimentalTxMock.json';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const provider = new ethers.JsonRpcProvider(process.env.PROVIDER_URI_GOERLI as string);
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY as string, provider);
+const axiomV1QueryExperimental = new ethers.Contract("0xCa057924353E372d7c46F194ed0d75eABA7f1ed1", axiomV1QueryExperimentalAbi.abi, wallet);
 
 const config: AxiomConfig = {
   providerUri: process.env.PROVIDER_URI_GOERLI || 'http://localhost:8545',
@@ -17,48 +19,44 @@ const config: AxiomConfig = {
 const ax = new Axiom(config);
 
 async function main() {
+  // Create a new Tx/Receipts Query
   let txqb = await newTxQuery();
-  console.log("Sending transactions Query transaction...");
+
+  // Send the Tx/Receipts query to experimental AxiomV1Query contract
+  console.log("Sending Tx/Receipts Query transaction...");
   const txRes = await txqb.sendTxReceiptsQuery(wallet, wallet.address, {
     value: ethers.parseEther("0.01"),
     gasPrice: ethers.parseUnits("100", "gwei"),
   });
   console.log(txRes);
 
-  let rqb = await newReceiptQuery();
-  console.log("Sending receipt Query transaction...");
-  const receiptRes = await rqb.sendOnlyReceiptsQuery(wallet, wallet.address, {
-    value: ethers.parseEther("0.01"),
-    gasPrice: ethers.parseUnits("100", "gwei"),
-  });
-  console.log(receiptRes);
+  // Listen for the response
+  axiomV1QueryExperimental.on("QueryFulfilled", fulfilledHandler);
 }
 
+// IMPORTANT: Each Query is hashed and used as a key in our AxiomV1Query contract, which means that 
+//            you MUST change the Query data in some way when submitting a a Query to the contract 
+//            or the transaction will fail.
 async function newTxQuery() {
   const qb = ax.experimental.newTxReceiptsQueryBuilder();
   await qb.appendTxQuery({
-    txHash:
-      "0x9ba6df3200fe8d62103ede64a32a2475e4c7f992fe5e8ea7f08a490edf32d48d",
+    txHash: "0x97f7f3354930dfd577f84b0db7022e14e8dbcf6c48fef7c84faa0ed8d22c9415",
     field: TransactionField.To,
   });
   await qb.appendTxQuery({
-    txHash:
-      "0x1320047d683efdf132b7939418a9f0061f9dfd7348a2f347f32745fb46cb6ec6",
+    txHash: "0x5a295694f62adf27f617e1d8d2e0e74e8f6c2464b7b7d86fadd2ac7548e8dede",
     field: TransactionField.Data,
   });
+  // await qb.appendReceiptQuery({
+  //   txHash: "0x5a295694f62adf27f617e1d8d2e0e74e8f6c2464b7b7d86fadd2ac7548e8dede",
+  //   field: ReceiptField.Status,
+  // });
   await qb.appendReceiptQuery({
-    txHash:
-      "0x1320047d683efdf132b7939418a9f0061f9dfd7348a2f347f32745fb46cb6ec6",
-    field: ReceiptField.Status,
-  });
-  await qb.appendReceiptQuery({
-    txHash:
-      "0x1320047d683efdf132b7939418a9f0061f9dfd7348a2f347f32745fb46cb6ec6",
+    txHash: "0x5a295694f62adf27f617e1d8d2e0e74e8f6c2464b7b7d86fadd2ac7548e8dede",
     field: ReceiptField.CumulativeGas,
   });
   await qb.appendReceiptQuery({
-    txHash:
-      "0x1320047d683efdf132b7939418a9f0061f9dfd7348a2f347f32745fb46cb6ec6",
+    txHash: "0x5a295694f62adf27f617e1d8d2e0e74e8f6c2464b7b7d86fadd2ac7548e8dede",
     field: ReceiptField.Logs,
     logIndex: 0,
   });
@@ -66,25 +64,26 @@ async function newTxQuery() {
   return qb;
 }
 
-async function newReceiptQuery() {
-  const qb = ax.experimental.newOnlyReceiptsQueryBuilder();
-  await qb.appendReceiptQuery({
-    txHash:
-      "0x1320047d683efdf132b7939418a9f0061f9dfd7348a2f347f32745fb46cb6ec6",
-    field: ReceiptField.Status,
-  });
-  await qb.appendReceiptQuery({
-    txHash:
-      "0x1320047d683efdf132b7939418a9f0061f9dfd7348a2f347f32745fb46cb6ec6",
-    field: ReceiptField.Logs,
-    logIndex: 0,
-  });
-  await qb.appendReceiptQuery({
-    txHash:
-      "0x1320047d683efdf132b7939418a9f0061f9dfd7348a2f347f32745fb46cb6ec6",
-    field: ReceiptField.CumulativeGas,
-  });
-  return qb;
+async function fulfilledHandler(queryType: number, keccakQueryResponse: string, payment: string, prover: string) {
+  console.log("QueryFulfilled", queryType, keccakQueryResponse, payment, prover);
+
+  const txqb = await newTxQuery();
+  const responseTree = await txqb.getResponseTrees();
+
+  // Get tx/Receipt response & data
+  const keccakTxResponse = responseTree.tx.tree.getHexRoot();
+  const keccakReceiptResponse = responseTree.receipt.tree.getHexRoot();
+  const txData = responseTree.tx.data;
+  const receiptData = responseTree.receipt.data;
+  responseTree.tx.data
+
+  console.log("keccakTxResponse", keccakTxResponse);
+  console.log("keccakReceiptResponse", keccakReceiptResponse);
+  console.log("txData", txData);
+  console.log("receiptData", receiptData);
+
+  // Send data to your smart contract to complete some action:
+  // yourSmartContract.claimTokens(numTokens, keccakTxResponse, keccakReceiptResponse, txData, receiptData);
 }
 
 main();
